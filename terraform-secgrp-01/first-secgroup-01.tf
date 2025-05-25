1691702993543ac6f20e511344f19065bde63de8
Jihad Hamza <jihadrh@gmail.com>
2025-05-25 22:47:35 +0400

    added sec group poc

---
+++ terraform-secgrp-01/first-secgroup-01.tf
@@ -0,0 +1,53 @@
+
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key"
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+  sensitive   = true
+}
+
+variable "aws_session_token" {
+  type        = string
+  description = "AWS Session Token"
+  sensitive   = true
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key=var.aws_access_key
+  secret_key=var.aws_secret_key
+  token=var.aws_session_token
+}
+
+data "aws_vpc" "main" {
+  id = "vpc-0916e9f9d22a5d549"
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = data.aws_vpc.main.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = data.aws_vpc.main.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
